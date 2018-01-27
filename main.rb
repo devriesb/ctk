111f82bcc379aacf2c827e4a1842c72ecd7623f6
John Michaels <johnmichaels@dhcp-10-16-1-79.pa.cloudera.com>
2018-01-26 16:53:16 -0800

    Trying for more readability

--- main.rb
+++ main.rb
@@ -30,7 +30,7 @@ class MiddleManager
     @conf.hosts.each do |host|
       puts "Testing connection: ssh#{@conf.user}@#{host}"
       @ssh_connection = Net::SSH.start(host, @conf.user, password: @conf.password)
-      result = x("hostname")
+      result = x "hostname"
       raise "Could not connect to #{host}" unless result =~ /#{host}/
     end
   end
@@ -73,45 +73,45 @@ class MiddleManager
 
   def set_swappiness
     puts "Setting 'swappiness' to lowest possible value (1)."
-    x("sh -c 'echo 1 > /proc/sys/vm/swappiness'")
+    x "sh -c 'echo 1 > /proc/sys/vm/swappiness'"
   end
 
   def disable_transparent_hugepage
-    thp_defrag_res = x("cat /sys/kernel/mm/transparent_hugepage/defrag")
-    thp_res        = x("cat /sys/kernel/mm/transparent_hugepage/enabled")
+    thp_defrag_res = x "cat /sys/kernel/mm/transparent_hugepage/defrag"
+    thp_res        = x "cat /sys/kernel/mm/transparent_hugepage/enabled"
 
     if thp_res =~ /\[never\]/ && thp_defrag_res =~ /\[never\]/
       puts "transparent_hugepage already disabled"
     else
       puts "Disabling transparent_hugepage"
-      x("echo never > /sys/kernel/mm/transparent_hugepage/enabled")
-      x("echo never > /sys/kernel/mm/transparent_hugepage/defrag")
+      x "echo never > /sys/kernel/mm/transparent_hugepage/enabled"
+      x "echo never > /sys/kernel/mm/transparent_hugepage/defrag"
 
       puts "Modifying /etc/rc.d/rc.local to disable transparent_hugepage on startup"
-      rc_local_file = x("cat /etc/rc.d/rc.local")
+      rc_local_file = x "cat /etc/rc.d/rc.local"
 
       unless rc_local_file =~ /\/sys\/kernel\/mm\/transparent_hugepage\/defrag/
         puts "Adding 'echo never > /sys/kernel/mm/transparent_hugepage/defrag'"
-        x("echo 'echo never > /sys/kernel/mm/transparent_hugepage/defrag' >> /etc/rc.d/rc.local")
+        x "echo 'echo never > /sys/kernel/mm/transparent_hugepage/defrag' >> /etc/rc.d/rc.local"
       end
 
       unless rc_local_file =~ /\/sys\/kernel\/mm\/transparent_hugepage\/enabled/
         puts "Adding 'echo never > /sys/kernel/mm/transparent_hugepage/enabled'"
-        x("echo 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' >> /etc/rc.d/rc.local")
+        x "echo 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' >> /etc/rc.d/rc.local"
       end
 
       puts "Ensuring /etc/rc.d/rc.local is executable"
-      x("chmod +x /etc/rc.d/rc.local")
+      x "chmod +x /etc/rc.d/rc.local"
     end
   end
 
   def ensure_nscd_running
-    res = x("service nscd status")
+    res = x "service nscd status"
     raise "ERROR: nscd not running on #{@host}" unless res =~ /active \(running\)/
   end
 
   def ensure_ntpd_running
-    res = x("service ntpd status")
+    res = x "service ntpd status"
     raise "ERROR: ntpd not running on #{@host}" unless res =~ /active \(running\)/
   end
 
@@ -122,24 +122,24 @@ class MiddleManager
       puts "Deploying /etc/my.cnf"
       scp("./files/mysql_#{master_or_slave}_config.my.cnf", "/etc/my.cnf")
       puts "Removing /var/lib/mysql/ib_logfile*"
-      x("rm -f /var/lib/mysql/ib_logfile*")
+      x "rm -f /var/lib/mysql/ib_logfile*"
       restart_mysql
     end
   end
 
   def install_maria_db
-    res = x("yum list installed | grep mariadb")
+    res = x "yum list installed | grep mariadb"
     if res =~ /mariadb-server/
       puts "MariaDB is already installed"
     else
       puts "Installing MariaDB (MySQL)"
 
       puts "yum install -y mariadb-server"
-      x("yum install -y mariadb-server")
+      x "yum install -y mariadb-server"
 
       puts "starting mariadb"
-      x("systemctl start mariadb")
-      x("systemctl enable mariadb")
+      x "systemctl start mariadb"
+      x "systemctl enable mariadb"
 
       restart_mysql
 
@@ -149,7 +149,7 @@ class MiddleManager
 
   def restart_mysql
     puts "Restarting mariadb service"
-    res = x("systemctl restart mariadb")
+    res = x "systemctl restart mariadb"
     if res.strip.length > 0
       puts "Error: \n"
       puts res.inspect
@@ -161,19 +161,19 @@ class MiddleManager
     puts "Securing MySQL installation"
 
     # Make sure that NOBODY can access the server without a password
-    x("mysql -e \"UPDATE mysql.user SET Password = PASSWORD('#{@conf.mysql_root_pass}') WHERE User = 'root'\"")
+    x "mysql -e \"UPDATE mysql.user SET Password = PASSWORD('#{@conf.mysql_root_pass}') WHERE User = 'root'\""
     # Kill the anonymous users
-    x("mysql -e \"DROP USER ''@'localhost'\"")
+    x "mysql -e \"DROP USER ''@'localhost'\""
     # Because our hostname varies we'll use some Bash magic here.
-    x("mysql -e \"DROP USER ''@'$(hostname)'\"")
+    x "mysql -e \"DROP USER ''@'$(hostname)'\""
     # Kill off the demo database
-    x("mysql -e \"DROP DATABASE test\"")
+    x "mysql -e \"DROP DATABASE test\""
     # Make our changes take effect
-    x("mysql -e \"FLUSH PRIVILEGES\"")
+    x "mysql -e \"FLUSH PRIVILEGES\""
 
-    x("echo [client] > ~/.my.cnf")
-    x("echo user=root >> ~/.my.cnf")
-    x("echo pass=#{@conf.mysql_root_pass} >> ~/.my.cnf")
+    x "echo [client] > ~/.my.cnf"
+    x "echo user=root >> ~/.my.cnf"
+    x "echo pass=#{@conf.mysql_root_pass} >> ~/.my.cnf"
   end
 
   def mysql_replication_setup(master_host, slave_host)
@@ -187,23 +187,23 @@ class MiddleManager
 
     puts "Creating slave user"
     @ssh_connection = Net::SSH.start(master_host, @conf.user, password: @conf.password)
-    x("mysql -e \"GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' IDENTIFIED BY '#{@conf.mysql_slave_user_pass}';\"")
-    x("mysql -e \"SET GLOBAL binlog_format = 'ROW';\"")
-    x("mysql -e \"FLUSH TABLES WITH READ LOCK;\"")
-    master_status = x("mysql -e \"SHOW MASTER STATUS;\"")
+    x "mysql -e \"GRANT REPLICATION SLAVE ON *.* TO 'slave_user'@'%' IDENTIFIED BY '#{@conf.mysql_slave_user_pass}';\""
+    x "mysql -e \"SET GLOBAL binlog_format = 'ROW';\""
+    x "mysql -e \"FLUSH TABLES WITH READ LOCK;\""
+    master_status = x "mysql -e \"SHOW MASTER STATUS;\""
 
     # master_status looks like:
     # "File\tPosition\tBinlog_Do_DB\tBinlog_Ignore_DB\nmysql_binary_log.000014\t2172\t\t\n"
     master_log_file = master_status.split("\n").last.split(" ").first
     master_log_pos  = master_status.split("\n").last.split(" ").last
 
-    x("mysql -e \"UNLOCK TABLES;\"")
+    x "mysql -e \"UNLOCK TABLES;\""
 
     puts "Configuring slave"
     @ssh = Net::SSH.start(slave_host, @conf.user, password: @conf.password)
-    x("mysql -e \"CHANGE MASTER TO MASTER_HOST='#{master_host}', MASTER_USER='slave_user', MASTER_PASSWORD='#{@conf.mysql_slave_user_pass}', MASTER_LOG_FILE='#{master_log_file}', MASTER_LOG_POS=#{master_log_pos};\"")
-    x("mysql -e \"START SLAVE;\"") 
-    x("mysql -e \"SHOW SLAVE STATUS\\G\"") 
+    x "mysql -e \"CHANGE MASTER TO MASTER_HOST='#{master_host}', MASTER_USER='slave_user', MASTER_PASSWORD='#{@conf.mysql_slave_user_pass}', MASTER_LOG_FILE='#{master_log_file}', MASTER_LOG_POS=#{master_log_pos};\""
+    x "mysql -e \"START SLAVE;\""
+    x "mysql -e \"SHOW SLAVE STATUS\\G\""
 
     puts "MySQL replication setup complete"
   end
@@ -226,11 +226,11 @@ class MiddleManager
     puts "Deploying /etc/profile.d/java.sh"
     scp("./files/java.sh", "/etc/profile.d/java.sh", @host)
 
-    x("chmod 744 /etc/profile.d/java.sh")
-    x("source /etc/profile.d/java.sh")
+    x "chmod 744 /etc/profile.d/java.sh"
+    x "source /etc/profile.d/java.sh"
 
     puts "Installing JDK 8 RPM"
-    x("yum localinstall -y /tmp/jdk-8u161-linux-x64.rpm")
+    x "yum localinstall -y /tmp/jdk-8u161-linux-x64.rpm"
   end
 
   def install_jdbc_driver
@@ -242,13 +242,13 @@ class MiddleManager
     puts "Installing JDBC driver - mysql-connector-java-5.1.45-bin.jar - in /usr/share/java"
     
     puts "Downloading JDBC driver"
-    puts x("wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.45.tar.gz")
-    puts x("tar -xzf mysql-connector-java-5.1.45.tar.gz")
+    x "wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.45.tar.gz"
+    x "tar -xzf mysql-connector-java-5.1.45.tar.gz"
 
     puts "Moving and renaming JDBC driver"
-    puts x("mkdir /usr/share/java")
-    puts x("mv mysql-connector-java-5.1.45/mysql-connector-java-5.1.45-bin.jar /usr/share/java/mysql-connector-java.jar")
-    puts x("rm -rf mysql-connector-java-5.1.45*")
+    x "mkdir /usr/share/java"
+    x "mv mysql-connector-java-5.1.45/mysql-connector-java-5.1.45-bin.jar /usr/share/java/mysql-connector-java.jar"
+    x "rm -rf mysql-connector-java-5.1.45*"
   end
 
   def setup_cm_dbs
@@ -256,24 +256,24 @@ class MiddleManager
     ['cmserver', 'hive', 'amon', 'rman', 'oozie', 'hue'].each do |db_name|
       puts "Creating #{db_name} database and #{db_name}_user user."
 
-      x("mysql -e \"CREATE DATABASE #{db_name} DEFAULT CHARACTER SET utf8;\"")
-      x("mysql -e \"DROP USER IF EXISTS '#{db_name}_user'@'%';\"")
-      x("mysql -e \"GRANT ALL on #{db_name}.* TO '#{db_name}_user'@'%' IDENTIFIED BY '#{@conf.mysql_cm_dbs_password}';\"")
+      x "mysql -e \"CREATE DATABASE #{db_name} DEFAULT CHARACTER SET utf8;\""
+      x "mysql -e \"DROP USER IF EXISTS '#{db_name}_user'@'%';\""
+      x "mysql -e \"GRANT ALL on #{db_name}.* TO '#{db_name}_user'@'%' IDENTIFIED BY '#{@conf.mysql_cm_dbs_password}';\""
     end
   end
 
   def install_cloudera_manager
-    puts x("yum install -y yum-utils")
-    puts x("yum-config-manager --add-repo https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/cloudera-manager.repo")
+    x "yum install -y yum-utils"
+    x "yum-config-manager --add-repo https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/cloudera-manager.repo"
     puts "Installing cloudera-manager-daemons"
-    puts x("yum install -y cloudera-manager-daemons")
-    puts x("yum install -y cloudera-manager-server")
+    x "yum install -y cloudera-manager-daemons"
+    x "yum install -y cloudera-manager-server"
 
     puts "Verifying Cloudera Manager databases are configured properly"
-    puts x("/usr/share/cmf/schema/scm_prepare_database.sh mysql cmserver cmserver_user #{@conf.mysql_cm_dbs_password}")
+    x "/usr/share/cmf/schema/scm_prepare_database.sh mysql cmserver cmserver_user #{@conf.mysql_cm_dbs_password}"
 
     puts "Starting cloudera-scm-server"
-    puts x("systemctl start cloudera-scm-server")
+    x "systemctl start cloudera-scm-server"
   end
 
   def x(cmd, verbose=false)
