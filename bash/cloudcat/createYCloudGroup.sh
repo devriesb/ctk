13ac0605dbea4bdb031523dd650aaceec2fa7d85
Brandon DeVries <bdevries@cloudera.com>
2020-04-13 21:18:46 -0400

    changing to centos

--- bash/cloudcat/createYCloudGroup.sh
+++ bash/cloudcat/createYCloudGroup.sh
@@ -5,7 +5,7 @@ source ./loadCredentials.sh
 path="$BASE_PATH/save"
 DATA_PATH="/tmp/data-$(date +%s)-$RANDOM.json"
 
-cat > "$DATA_PATH" << EOF
+cat >"$DATA_PATH" <<EOF
 {
   "username": "$USER_NAME",
   "apiKey": "$USER_API_KEY",
@@ -15,7 +15,7 @@ cat > "$DATA_PATH" << EOF
   "sendEmail": "FALSE",
   "description": "",
   "cloudType": "4",
-  "imageName": "registry.eng.hortonworks.com/hortonworks/base-ubuntu16:0.1.0.0-92",
+  "imageName": "registry.eng.hortonworks.com/hortonworks/base-centos7.7:0.1.0.0-95",
   "ycloudQueue": "default-developers",
   "initScript": "$USER_DATA $2",
   "primarySize": "cpus_01_ramGB_004",
@@ -25,8 +25,8 @@ cat > "$DATA_PATH" << EOF
 }
 EOF
 curl -H "Accept: application/json" \
-     -H "Content-type: application/json" \
-     -d "@$DATA_PATH" \
-     -X POST $path
+  -H "Content-type: application/json" \
+  -d "@$DATA_PATH" \
+  -X POST $path
 
 rm "$DATA_PATH"
