4d5def5387fed0ad851fbd8cf87d948a1f4ac405
Brandon DeVries <bdevries@cloudera.com>
2020-04-14 12:34:36 -0400

    adding jq details

--- bash/cloudcat/singleGroupInfo.sh
+++ bash/cloudcat/singleGroupInfo.sh
@@ -2,6 +2,12 @@
 
 source ./loadCredentials.sh
 
-path="$BASE_PATH/show/$1?\
-username=$USER_NAME&apiKey=$USER_API_KEY"
-curl -H "Accept: application/json" -H "Content-type: application/json" -X GET $path
+path="$BASE_PATH/show/$1?username=$USER_NAME&apiKey=$USER_API_KEY"
+jsonResponse=$(curl -H "Accept: application/json" -H "Content-type: application/json" -X GET "$path")
+
+echo "Response JSON: $jsonResponse"
+
+echo ""
+
+echo "Public IPs:"
+echo "$jsonResponse"|jq '.provisionedInstances[].publicIp'
